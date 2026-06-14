2f9e665051ffffef90dbd265ecff6af5181d6479
unknown <gdutt2304@gmail.com>
2026-06-14 11:38:21 +0530

    break points

---
+++ project3/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <div className="logo">logo</div>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Project</li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
