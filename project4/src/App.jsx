b0a1621b8a71abd4a7f79649cc2933f8c404a699
unknown <gdutt2304@gmail.com>
2026-06-16 11:29:51 +0530

    propes

---
+++ project4/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import File1 from "./components/File1";
+
+function App() {
+  return (
+    <>
+      <h1>hello world </h1>
+      <File1 name="Ganesh Dutt" />
+      <File1 name="krishna Dutt" />
+      <File1 name="Rahul Dutt" />
+      <File1 name="Ganesh Dutt" />
+      <File1 name="Ganesh Dutt" />
+      <File1 name="Ganesh Dutt" />
+    </>
+  );
+}
+
+export default App;
---
+++ project4/src/components/File1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const File1 = (props) => {
+  return (
+    <div>
+      <h1>Hey Good morning : {props.name} </h1>
+    </div>
+  );
+};
+
+export default File1;
