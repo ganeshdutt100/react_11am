174a1db38bde8e6c4de94cc2864343e0bfc49583
unknown <gdutt2304@gmail.com>
2026-06-16 11:30:15 +0530

    props

--- project4/src/components/File1.jsx
+++ project4/src/components/File1.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const File1 = (props) => {
   return (
     <div>
-      <h1>Hey Good morning : {props.name} </h1>
+      <h1>Hey Good morning :{props.name} </h1>
     </div>
   );
 };
