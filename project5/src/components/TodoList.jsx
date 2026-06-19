1f7246702174c37f726312d59071f3b674ad6878
unknown <gdutt2304@gmail.com>
2026-06-19 12:04:06 +0530

    ToDoList

--- project5/src/components/TodoList.jsx
+++ project5/src/components/TodoList.jsx
@@ -6,8 +6,14 @@ const TodoList = () => {
 
   const userHandler = () => {
     if (input.trim() === "") return alert("Please enter your value ");
-    setList(...list, input);
+    setList([...list, input]);
     setInput("");
+  };
+
+  const enterValue = (e) => {
+    if (e.key === "Enter") {
+      userHandler();
+    }
   };
 
   return (
@@ -19,11 +25,17 @@ const TodoList = () => {
         className="border mx-5 mt-5"
         value={input}
         onChange={(e) => setInput(e.target.value)}
+        onKeyDown={enterValue}
       />
       <button onClick={userHandler}>Add</button>
       <div className="m-10">
         {list.map((item) => (
-          <div key={item}>{item}</div>
+          <div key={item}>
+            {item}
+            <button onClick={() => setList(list.filter((x) => x !== item))}>
+              ❌
+            </button>
+          </div>
         ))}
       </div>
     </>
