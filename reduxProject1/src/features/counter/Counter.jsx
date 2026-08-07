d65db73fe29beee1136e7042ef917e884eec4946
Ganesh Dutt <gdutt2304@gmail.com>
2026-08-07 12:38:13 +0530

    reduxt tookit

---
+++ reduxProject1/src/features/counter/Counter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {increment , decrement , reset} from './counterSlice'
+const Counter = () => {
+  return (
+    <div>Counter</div>
+  )
+}
+
+export default Counter
