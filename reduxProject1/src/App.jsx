6b7422b60e41155066069e09192ec8780f858984
Ganesh Dutt <gdutt2304@gmail.com>
2026-08-08 12:43:42 +0530

    reduxToolkit

--- reduxProject1/src/App.jsx
+++ reduxProject1/src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+import Counter from "./features/counter/Counter";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <h1>React.js</h1>
-     </>
-  )
+      <h1>React.js</h1>
+      <Counter />
+    </>
+  );
 }
 
-export default App
+export default App;
--- reduxProject1/src/features/counter/Counter.jsx
+++ reduxProject1/src/features/counter/Counter.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import {increment , decrement , reset} from './counterSlice'
+import React from "react";
+import { increment, decrement, reset } from "./counterSlice";
+import { useSelector, useDispatch } from "react-redux";
 const Counter = () => {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
   return (
-    <div>Counter</div>
-  )
-}
+    <div>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <span>{count}</span>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(reset())}>Reset</button>
+    </div>
+  );
+};
 
-export default Counter
+export default Counter;
